2748c319afc79a6c600d2712ec19551c803f20af
ozippy <jeremy@ozippy.com>
2019-07-20 15:41:29 +1000

    first commit

---
+++ src/webparts/groupNavigation/GroupNavigationWebPart.ts
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+import { Version } from '@microsoft/sp-core-library';
+import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
+import {
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField
+} from '@microsoft/sp-property-pane';
+
+import * as strings from 'GroupNavigationWebPartStrings';
+import GroupNavigation from './components/GroupNavigation';
+import { IGroupNavigationProps } from './components/IGroupNavigationProps';
+
+export interface IGroupNavigationWebPartProps {
+  description: string;
+}
+
+export default class GroupNavigationWebPart extends BaseClientSideWebPart<IGroupNavigationWebPartProps> {
+
+  public render(): void {
+    const element: React.ReactElement<IGroupNavigationProps > = React.createElement(
+      GroupNavigation,
+      {
+        description: this.properties.description
+      }
+    );
+
+    ReactDom.render(element, this.domElement);
+  }
+
+  protected onDispose(): void {
+    ReactDom.unmountComponentAtNode(this.domElement);
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
---
+++ src/webparts/groupNavigation/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IGroupNavigationWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'GroupNavigationWebPartStrings' {
+  const strings: IGroupNavigationWebPartStrings;
+  export = strings;
+}
