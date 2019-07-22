1084f87c36ea25f5ffac1c617f2c0af339ebc762
ozippy <jeremy@ozippy.com>
2019-07-22 18:02:58 +1000

    Refactored into state component

--- src/webparts/groupNavigation/components/FunctionComponents/NavList.tsx
+++ src/webparts/groupNavigation/components/FunctionComponents/NavList.tsx
@@ -7,110 +7,31 @@ import ISPNavItem from './INavItem';
 import useNavState from './NavState';
 import { Nav, INavLinkGroup } from 'office-ui-fabric-react/lib/Nav';
 import { Spinner, SpinnerSize } from 'office-ui-fabric-react/lib/Spinner';
-import { INavGroup, IGroupLink } from './INavItem'
+import { INavGroup, IGroupLink } from './INavItem';
 
 
 const initialEntity = "companyb.com";
 
 export default function NavList() {
 
-  const { navItems, isLoading } = useNavState(initialEntity);
-
-  let groupItems: INavGroup[] = [];
-  let groupItem: INavGroup = null;
-  let currentGroup: string = "";
-
-
-  //convert the sharepoint items to the format needed by the office ui fabric Nav control
-  if (navItems.length > 0) {
-    for (let i = 0; i < navItems.length; i++) {
-      if (navItems[i].NavigationGroup !== currentGroup) {
-        if (currentGroup !== "") {
-          groupItems.push(groupItem);
-        }
-        currentGroup = navItems[i].NavigationGroup;
-        groupItem = { name: navItems[i].NavigationGroup, links: [] };
-      }
-      let currentLink: IGroupLink = { name: navItems[i].Title, url: navItems[i].LinkURL, key: navItems[i].Id.toString(), target: navItems[i].Target };
-      groupItem.links.push(currentLink);
-      if (i === navItems.length - 1) {
-        groupItems.push(groupItem);
-      }
-    }
-    console.log(JSON.stringify(groupItems));
-  };
-
-
+  const { navItemsGroup, isLoading } = useNavState(initialEntity);
 
   return (
     <div className={styles.groupNavigation}>
       <p>List</p>
       <EntitySelector />
-      {isLoading ? <Spinner size={SpinnerSize.medium} /> : ''}
+      {isLoading ? <Spinner size={SpinnerSize.large} /> : ''}
 
       <div>
         <Nav
           onRenderGroupHeader={_onRenderGroupHeader}
-          groups={groupItems}
+          groups={navItemsGroup}
         />
       </div>
     </div>
-
-
-
-
   );
 
 }
-
-
-// [
-//   {
-//     name: 'Pages',
-//     links: [
-//       // prettier-ignore
-//       { name: 'Activity', url: 'http://msn.com', key: 'key1', target: '_blank' },
-//       { name: 'News', url: 'http://msn.com', key: 'key2', target: '_blank' }
-//     ]
-//   },
-//   {
-//     name: 'More pages',
-//     links: [
-//       // prettier-ignore
-//       { name: 'Settings', url: 'http://msn.com', key: 'key3', target: '_blank' },
-//       { name: 'Notes', url: 'http://msn.com', key: 'key4', target: '_blank' }
-//     ]
-//   }
-// ]
-
-// import * as React from 'react';
-// import { Nav, INavLinkGroup } from '../index';
-
-// export const NavCustomGroupHeadersExample: React.StatelessComponent = () => {
-//   return (
-//     <Nav
-//       onRenderGroupHeader={_onRenderGroupHeader}
-//       groups={[
-//         {
-//           name: 'Pages',
-//           links: [
-//             // prettier-ignore
-//             { name: 'Activity', url: 'http://msn.com', key: 'key1', target: '_blank' },
-//             { name: 'News', url: 'http://msn.com', key: 'key2', target: '_blank' }
-//           ]
-//         },
-//         {
-//           name: 'More pages',
-//           links: [
-//             // prettier-ignore
-//             { name: 'Settings', url: 'http://msn.com', key: 'key3', target: '_blank' },
-//             { name: 'Notes', url: 'http://msn.com', key: 'key4', target: '_blank' }
-//           ]
-//         }
-//       ]}
-//     />
-//   );
-// };
 
 function _onRenderGroupHeader(group: INavLinkGroup): JSX.Element {
   return <h3>{group.name}</h3>;
--- src/webparts/groupNavigation/components/FunctionComponents/NavState.tsx
+++ src/webparts/groupNavigation/components/FunctionComponents/NavState.tsx
@@ -4,6 +4,7 @@ import styles from '../GroupNavigation.module.scss';
 import ISPNavItem from './INavItem';
 import { sp } from "@pnp/sp";
 import { find } from '@microsoft/sp-lodash-subset';
+import { INavGroup, IGroupLink } from './INavItem';
 
 // const testData: INavItem[] = [{ id: 1, title: "Google", entity: "sanitarium.com.au", navGroup: "Do", linkUrl: "https://google.com", target: '_blank' },
 // { id: 2, title: "Microsoft", entity: "sanitarium.com.au", navGroup: "Discover", linkUrl: "https://microsoft.com", target: '_blank' }];
@@ -14,8 +15,11 @@ export default function useNavState(selectedEntity: string) {
     //set a default entity
     const [entity, setEntity] = React.useState(selectedEntity);
     //init state to empty array for nav items
-    const [navItems, setNavItems] = React.useState<ISPNavItem[]>([]);
+    const [navItemsGroup, setNavItemsGroup] = React.useState<INavGroup[]>([]);
 
+    let groupItems: INavGroup[] = [];
+    let groupItem: INavGroup = null;
+    let currentGroup: string = "";
 
     //Load the data and filter out items for only the selectede entity
     const loadNavItems = () => {
@@ -28,25 +32,43 @@ export default function useNavState(selectedEntity: string) {
                 if (includeFlag) return item;
             });
 
-            //console.log(JSON.stringify(filteredItems));
 
-            filteredItems.forEach(element => {
-                console.log(element.Id);
-                console.log(element.Title);
-                console.log(element.Entity);
-                console.log(element.NavigationGroup);
-                console.log(element.LinkURL);
-            });
+            //convert the sharepoint items to the format needed by the office ui fabric Nav control
+            if (filteredItems.length > 0) {
+                for (let i = 0; i < filteredItems.length; i++) {
+                    if (filteredItems[i].NavigationGroup !== currentGroup) {
+                        if (currentGroup !== "") {
+                            groupItems.push(groupItem);
+                        }
+                        currentGroup = filteredItems[i].NavigationGroup;
+                        groupItem = { name: filteredItems[i].NavigationGroup, links: [] };
+                    }
+                    let currentLink: IGroupLink = { name: filteredItems[i].Title, url: filteredItems[i].LinkURL, key: filteredItems[i].Id.toString(), target: filteredItems[i].Target };
+                    groupItem.links.push(currentLink);
+                    if (i === filteredItems.length - 1) {
+                        groupItems.push(groupItem);
+                    }
+                }
+                // console.log(JSON.stringify(groupItems));
+            }
+
+            // filteredItems.forEach(element => {
+            //     console.log(element.Id);
+            //     console.log(element.Title);
+            //     console.log(element.Entity);
+            //     console.log(element.NavigationGroup);
+            //     console.log(element.LinkURL);
+            // });
             //Populate the navItems from the SharePoint list
-            setNavItems(filteredItems);
+            setNavItemsGroup(groupItems);
             setLoading(false);
         });
-    }
+    };
 
     //When the entity changes, reload the nav items
     React.useEffect(() => {
         loadNavItems();
-    }, [selectedEntity])
+    }, [selectedEntity]);
 
-    return { navItems, isLoading };
+    return { navItemsGroup, isLoading };
 }
